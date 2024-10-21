92411c40d0b5be2f0ea23d3e2af1427a426f13d4
Daniel Vong <dvwl@hotmail.com>
2024-10-21 19:41:18 +0800

    Update Program.cs
    Fix formatting and updated List declaration

--- demos/01-linq/1.10-aggregate/Program.cs
+++ demos/01-linq/1.10-aggregate/Program.cs
@@ -4,19 +4,19 @@ namespace Aggregate;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        List<Student> students = new List<Student>
-		{
+	static void Main(string[] args)
+	{
+		List<Student> students =
+		[
 			new Student { FirstName = "John", LastName = "Doe", ID = 1, Year = GradeLevel.FirstYear, Scores = new List<int>{90, 80, 70}, DepartmentID = 1 },
 			new Student { FirstName = "Jane", LastName = "Smith", ID = 2, Year = GradeLevel.SecondYear, Scores = new List<int>{85, 90, 75}, DepartmentID = 2 },
 			new Student { FirstName = "Jake", LastName = "Doe", ID = 3, Year = GradeLevel.ThirdYear, Scores = new List<int>{88, 70, 92}, DepartmentID = 3 },
 			new Student { FirstName = "Tony", LastName = "Field", ID = 4, Year = GradeLevel.ThirdYear, Scores = new List<int>{90, 80, 88}, DepartmentID = 1 },
 			new Student { FirstName = "Sharon", LastName = "Green", ID = 5, Year = GradeLevel.ThirdYear, Scores = new List<int>{96, 95, 70}, DepartmentID = 1 },
 			new Student { FirstName = "Daren", LastName = "Smile", ID = 6, Year = GradeLevel.FourthYear, Scores = new List<int>{90, 99, 89}, DepartmentID = 2 },
-		};
+		];
 
-        // Aggregate Operators
+		// Aggregate Operators
 		// Count: Total number of students
 		int studentCount = students.Count();
 		Console.WriteLine($"Total number of students: {studentCount}");
@@ -39,7 +39,7 @@ class Program
 
 		// Aggregate: Concatenate all student names into a single string
 		string allNames = students.Select(s => s.FirstName + " " + s.LastName)
-		                          .Aggregate((current, next) => current + ", " + next);
+								  .Aggregate((current, next) => current + ", " + next);
 		Console.WriteLine($"All student names: {allNames}");
-    }
+	}
 }
