801d38a781400c49bfb19e6286dd65ff42bc113a
Daniel Vong <dvwl@hotmail.com>
2026-05-18 08:45:48 +0800

    added timestamp

--- demos/05-parallelism/5.2-multithreading/Program.cs
+++ demos/05-parallelism/5.2-multithreading/Program.cs
@@ -4,34 +4,34 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello multithreading: tasks, threads, and threadpool!");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Hello multithreading: tasks, threads, and threadpool!");
 
 		// Creating and running a task
 		Task task = Task.Run(() =>
 		{
-			Console.WriteLine("Task is running on thread: " + Thread.CurrentThread.ManagedThreadId);
+			Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task is running on thread: {Thread.CurrentThread.ManagedThreadId}");
 		});
 
 		// Waiting for the task to complete
 		task.Wait();
-		Console.WriteLine("Task completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task completed.");
 
 		// Creating and starting a thread
 		Thread thread = new Thread(() =>
 		{
-			Console.WriteLine("Thread is running on thread: " + Thread.CurrentThread.ManagedThreadId);
+			Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Thread is running on thread: {Thread.CurrentThread.ManagedThreadId}");
 		});
 
 		// Start the thread
 		thread.Start();
 		// Wait for the thread to finish
 		thread.Join();
-		Console.WriteLine("Thread completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Thread completed.");
 
 		// Queuing work to the ThreadPool
 		ThreadPool.QueueUserWorkItem(state =>
 		{
-			Console.WriteLine("Task is running in ThreadPool on thread: " + Thread.CurrentThread.ManagedThreadId);
+			Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task is running in ThreadPool on thread: {Thread.CurrentThread.ManagedThreadId}");
 		});
 
 		// Queueing work items
@@ -40,16 +40,16 @@ class Program
             _ = ThreadPool.QueueUserWorkItem(DoWork, i);
 		}
 
-		Console.WriteLine("Main thread does some work.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Main thread does some work.");
 		Thread.Sleep(1000); // Simulate some work
 
-		Console.WriteLine("Main thread ends.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Main thread ends.");
 	}
 
 	static void DoWork(object state)
 	{
 		int threadNumber = (int)state;
-		Console.WriteLine($"Thread {threadNumber} is executing on thread: {Thread.CurrentThread.ManagedThreadId}");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Thread {threadNumber} is executing on thread: {Thread.CurrentThread.ManagedThreadId}");
 		Thread.Sleep(500); // Simulate work
 	}
 }
--- demos/05-parallelism/5.4-async-await/Program.cs
+++ demos/05-parallelism/5.4-async-await/Program.cs
@@ -5,7 +5,7 @@ class Program
 	static async Task Main(string[] args)
 	{
 		// Execute the methods
-		Console.WriteLine("Starting async tasks...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Starting async tasks...");
 
 		// Simulate a long calculation task
 		await CalculateAsync();
@@ -16,15 +16,15 @@ class Program
 		// Run tasks sequentially
 		await RunTasksSequentiallyAsync();
 
-		Console.WriteLine("Async tasks completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Async tasks completed.");
 	}
 
 	// Simulates a long-running calculation task
 	public static int LongCalculation()
 	{
-		Console.WriteLine("Starting LongCalculation...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Starting LongCalculation...");
 		Thread.Sleep(3000); // Simulate a delay
-		Console.WriteLine("LongCalculation completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] LongCalculation completed.");
 		return 42; // Return a result after the calculation
 	}
 
@@ -32,52 +32,52 @@ class Program
 	public static async Task<int> CalculateAsync()
 	{
 		int result = await Task.Run(() => LongCalculation());
-		Console.WriteLine($"Calculation result: {result}");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Calculation result: {result}");
 		return result;
 	}
 
 	// An async method that performs Task1 and Task2 asynchronously
 	public static async Task PerformTaskAsync()
 	{
-		Console.WriteLine("Performing Task1 asynchronously...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Performing Task1 asynchronously...");
 		await Task.Run(() => Task1());
 
-		Console.WriteLine("Performing Task2 asynchronously...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Performing Task2 asynchronously...");
 		await Task.Run(() => Task2());
 	}
 
 	// Simulate tasks that will be run sequentially
 	public static async Task RunTasksSequentiallyAsync()
 	{
-		Console.WriteLine("Running tasks sequentially...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Running tasks sequentially...");
 		await Task1();
 		await Task2();
-		Console.WriteLine("Sequential tasks completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Sequential tasks completed.");
 	}
 
 	// Simulate tasks that will be run concurrently
 	public static async Task RunTasksConcurrentlyAsync()
 	{
-		Console.WriteLine("Running tasks concurrently...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Running tasks concurrently...");
 		var task1 = Task1();
 		var task2 = Task2();
 		await Task.WhenAll(task1, task2);
-		Console.WriteLine("Concurrent tasks completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Concurrent tasks completed.");
 	}
 
 	// Simulate Task1
 	public static async Task Task1()
 	{
-		Console.WriteLine("Task1 starting...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task1 starting...");
 		await Task.Delay(2000); // Simulates async work with a 2-second delay
-		Console.WriteLine("Task1 completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task1 completed.");
 	}
 
 	// Simulate Task2
 	public static async Task Task2()
 	{
-		Console.WriteLine("Task2 starting...");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task2 starting...");
 		await Task.Delay(3000); // Simulates async work with a 3-second delay
-		Console.WriteLine("Task2 completed.");
+		Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Task2 completed.");
 	}
 }
